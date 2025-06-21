efc6d74b603680a0ef0195c58799a9bad8f10524
syunki-16 <syunki16_minions@yahoo.co.jp>
2025-06-21 10:08:39 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -241,5 +241,9 @@ TEMPLATE_DELETE = """
 """
 
 # アプリ起動
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render用にPORT環境変数を使う
+    app.run(debug=True, host="0.0.0.0", port=port)
+
